e0dd17d81772fa8bb667b52b9066f6c62f52e264
shuckiy73 <shuckiy73@yandex.ru>
2024-05-12 22:48:03 +0300

    modified

--- testapp/main/views.py
+++ testapp/main/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'BMW',
+            'age': 51,
+            'hobby': 'Workout'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
